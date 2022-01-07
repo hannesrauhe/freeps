6aefa35dd9e5303ee2001fb6d28728cb76ff0848
Hannes <mail+github@rauhe.eu>
2022-01-07 21:53:09 +0100

    feat(restonatorx) add raspistill handler

--- restonatorx/restonatorx.go
+++ restonatorx/restonatorx.go
@@ -8,12 +8,50 @@ import (
 	"net/http"
 	"net/url"
 	"os/exec"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/hannesrauhe/freeps/freepslib"
 	"github.com/hannesrauhe/freeps/utils"
 )
+
+func CaptureRaspiStill(width, height int, cameraParams map[string]interface{}) (bytes []byte, err error) {
+	args := []string{
+		"-w", strconv.Itoa(width),
+		"-h", strconv.Itoa(height),
+		"-o", "-", // output to stdout
+	}
+	for k, v := range cameraParams {
+		args = append(args, k)
+		if v != nil {
+			args = append(args, fmt.Sprintf("%v", v))
+		}
+	}
+
+	byt, err := exec.Command("/usr/bin/raspistill", args...).CombinedOutput()
+	if err != nil {
+		log.Printf("*** Error running raspistillbin: %v\n", err)
+		return []byte{}, err
+	}
+	return byt, nil
+}
+
+func RaspiHandler(w http.ResponseWriter, r *http.Request) {
+	bytes, err := CaptureRaspiStill(1600, 1200, map[string]interface{}{"--quality": 90, "--brightness": 50})
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error executing raspistill: %v", string(err.Error()))
+	}
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.Header().Set("Content-Length", strconv.Itoa(len(bytes)))
+	if _, err := w.Write(bytes); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "unable to write image to response: %v", string(err.Error()))
+	}
+}
 
 func ExecHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -135,7 +173,7 @@ func main() {
 	r.HandleFunc("/exec/{script:[a-z0-9_]+}/{arg:[a-z0-9_]+}", ExecHandler)
 	r.HandleFunc("/script/{script:[a-z0-9_]+}/{arg:[a-z0-9_]+}", ExecHandler)
 	r.HandleFunc("/denon/{function}", DenonHandler)
-
+	r.HandleFunc("/raspistill", RaspiHandler)
 	r.Handle("/fritz/{function}", fh)
 	r.Handle("/fritz/{function}/{device}", fh)
 
