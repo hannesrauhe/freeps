8749375906634a1a287eefa351f38e6d29f9f125
Hannes Rauhe <mail+github@rauhe.eu>
2022-02-07 23:03:49 +0100

    feat(freepslib): re-use freepslib auth for multiple statemnts

--- freepsdo/fritzmod.go
+++ freepsdo/fritzmod.go
@@ -12,6 +12,7 @@ import (
 )
 
 type FritzMod struct {
+	fl  *freepslib.Freeps
 	fc  *freepslib.FBconfig
 	ffc *freepsflux.FreepsFluxConfig
 }
@@ -33,29 +34,25 @@ func NewFritzMod(cr *utils.ConfigReader) *FritzMod {
 	if err != nil {
 		log.Print(err)
 	}
-	return &FritzMod{&conf, &ffc}
+	f, _ := freepslib.NewFreepsLib(&conf)
+	return &FritzMod{fl: f, fc: &conf, ffc: &ffc}
 }
 
 func (m *FritzMod) DoWithJSON(fn string, jsonStr []byte, w http.ResponseWriter) {
+	var err error
 	var vars map[string]string
 	json.Unmarshal(jsonStr, &vars)
-	f, err := freepslib.NewFreepsLib(m.fc)
-	if err != nil {
-		fmt.Fprintf(w, "FritzMod\nParameters: %v\nError on freepslib-init: %v", vars, string(err.Error()))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 
 	if fn == "freepsflux" {
-		ff, err2 := freepsflux.NewFreepsFlux(m.ffc, f)
+		ff, err2 := freepsflux.NewFreepsFlux(m.ffc, m.fl)
 		if err2 != nil {
 			log.Fatalf("Error while executing function: %v\n", err2)
 		}
-		ff.Verbose = f.Verbose
+		ff.Verbose = m.fl.Verbose
 		err = ff.Push()
 		return
 	} else if fn == "getdevicelistinfos" {
-		devl, err := f.GetDeviceList()
+		devl, err := m.fl.GetDeviceList()
 		if err != nil {
 			fmt.Fprintf(w, "FritzHandler\nParameters: %v\nError when getting device list: %v", vars, string(err.Error()))
 			w.WriteHeader(http.StatusInternalServerError)
@@ -74,9 +71,9 @@ func (m *FritzMod) DoWithJSON(fn string, jsonStr []byte, w http.ResponseWriter) 
 	dev := vars["device"]
 	if fn == "wakeup" {
 		log.Printf("Waking Up %v", dev)
-		err = f.WakeUpDevice(dev)
+		err = m.fl.WakeUpDevice(dev)
 	} else {
-		err = f.HomeAutoSwitch(fn, dev, vars)
+		err = m.fl.HomeAutoSwitch(fn, dev, vars)
 	}
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
