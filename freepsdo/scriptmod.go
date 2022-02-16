b0b6cc42aa25c8fd48a742ec37acdd7bc8235884
Hannes Rauhe <hannesrauhe@users.noreply.github.com>
2022-02-16 20:24:51 +0100

    feat(freepsd) enable script mod (#9)
    * possibility to execute scripts from a pre-defined directory

--- freepsdo/scriptmod.go
+++ freepsdo/scriptmod.go
@@ -3,30 +3,68 @@ package freepsdo
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os/exec"
+	"path"
+
+	"github.com/hannesrauhe/freeps/utils"
 )
 
 type ScriptMod struct {
+	ScriptDir string
 }
+
+type ScriptModConfig struct {
+	ScriptDir string
+}
+
+var ScriptModDefaultConfig = ScriptModConfig{"/etc/freepsd/scripts"}
 
 var _ Mod = &ScriptMod{}
 
 type ScriptParameters struct {
+	Mode string
 	Args []string
+}
+
+func NewScriptMod(cr *utils.ConfigReader) *ScriptMod {
+	conf := ScriptModDefaultConfig
+	err := cr.ReadSectionWithDefaults("scriptmod", &conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cr.WriteBackConfigIfChanged()
+	if err != nil {
+		log.Print(err)
+	}
+	return &ScriptMod{ScriptDir: conf.ScriptDir}
 }
 
 func (m *ScriptMod) DoWithJSON(function string, jsonStr []byte, w http.ResponseWriter) {
 	params := ScriptParameters{}
-	json.Unmarshal(jsonStr, &params)
-	cmd := exec.Command("./scripts/"+function, params.Args...)
-	stdout, err := cmd.Output()
+	err := json.Unmarshal(jsonStr, &params)
+
+	if err != nil {
+		log.Printf("%q", jsonStr)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	scriptName := path.Base(function)
+	cmd := exec.Command(m.ScriptDir+"/"+scriptName, params.Args...)
+	var stdout []byte
+	if params.Mode == "detach" {
+		err = cmd.Start()
+	} else {
+		stdout, err = cmd.Output()
+	}
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Executed: %v\nParameters: %v\nError: %v", function, params.Args, string(err.Error()))
+		fmt.Fprintf(w, "Executed: %v\nParameters: %v\nError: %v", scriptName, params.Args, string(err.Error()))
 	} else {
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "Executed: %v\nParameters: %v\nOutput: %v", function, params.Args, string(stdout))
+		fmt.Fprintf(w, "Executed: %v\nParameters: %v\nOutput: %v", scriptName, params.Args, string(stdout))
 	}
 
 }
