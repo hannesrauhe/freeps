ad0e0e4f073d0c2630c761055c27196ccb441524
Hannes Rauhe <mail+github@rauhe.eu>
2022-02-26 11:13:19 +0100

    feat: distribute config

--- utils/config.go
+++ utils/config.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"sync"
@@ -49,58 +51,44 @@ func MergeJsonWithDefaults(jsonBytes []byte, configStruct interface{}) error {
 }
 
 type ConfigIncluder struct {
-	Include string
+	IncludeFromFile string
+	IncludeFromURL  string
 }
 
-// func TemplatesFromUrl(url string) map[string]Template {
-// 	t := map[string]Template{}
-// 	c := http.Client{}
-// 	resp, err := c.Get(url)
-// 	if err != nil {
-// 		log.Printf("Error when reading from %v: %v", url, err)
-// 		return t
-// 	}
-// 	if resp.StatusCode > 300 {
-// 		log.Printf("Error when reading from %v: Status code %v", url, resp.StatusCode)
-// 		return t
-// 	}
-// 	byt, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		log.Printf("Error when reading from %v: %v", url, err)
-// 		return t
-// 	}
-// 	err = json.Unmarshal(byt, &t)
-// 	if err != nil {
-// 		log.Printf("Error when parsing json: %v\n %q", err, byt)
-// 	}
-
-// 	return t
-// }
-
-// func TemplatesFromFile(path string) map[string]Template {
-// 	t := map[string]Template{}
-// 	byt, err := ioutil.ReadFile(path)
-// 	if err != nil {
-// 		log.Printf("Error when reading from %v: %v", path, err)
-// 		return t
-// 	}
-// 	err = json.Unmarshal(byt, &t)
-// 	if err != nil {
-// 		log.Printf("Error when parsing json: %v\n %q", err, byt)
-// 	}
-// 	return t
-// }
-
-///TryIncluding check if sectionBytes contains the include keyword. If it does TryIncluding replaces sectionBytes with the content of the given path
-func TryInlcuding(sectionBytes []byte) {
-	var ci ConfigIncluder
-	err := json.Unmarshal(sectionBytes, &ci)
-	if err != nil || len(ci.Include) == 0 {
-		return
+func ReadBytesFromUrl(url string) []byte {
+	var byt []byte
+	c := http.Client{}
+	resp, err := c.Get(url)
+	if err != nil {
+		log.Printf("Error when reading from %v: %v", url, err)
+		return []byte{}
 	}
-
+	if resp.StatusCode > 300 {
+		log.Printf("Error when reading from %v: Status code %v", url, resp.StatusCode)
+		return []byte{}
+	}
+	byt, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error when reading from %v: %v", url, err)
+		return []byte{}
+	}
+	return byt
 }
 
+func ReadBytesFromFile(path string) []byte {
+	var err error
+	var byt []byte
+	byt, err = ioutil.ReadFile(path)
+	if err != nil {
+		log.Printf("Error when reading from %v: %v", path, err)
+		return []byte{}
+	}
+	return byt
+}
+
+// ReadSectionWithDefaults parses the content of the first-level-JSON object in <sectionName> into configStruct
+// if the section exists, the content is merged with configStruct;
+// if the section does not exist, the serialized content of configStruct is returned (assuming these are default values)
 func ReadSectionWithDefaults(jsonBytes []byte, sectionName string, configStruct interface{}) ([]byte, error) {
 	sectionsMap := make(map[string]interface{})
 	var err error
@@ -113,6 +101,7 @@ func ReadSectionWithDefaults(jsonBytes []byte, sectionName string, configStruct 
 	}
 
 	if sectionsMap[sectionName] == nil {
+		// section is missing, will include the default in the JSON string
 		sectionsMap[sectionName], err = StructToMap(configStruct)
 		if err != nil {
 			return []byte{}, err
@@ -124,7 +113,21 @@ func ReadSectionWithDefaults(jsonBytes []byte, sectionName string, configStruct 
 		return []byte{}, err
 	}
 
-	TryInlcuding(sectionBytes)
+	// checking if the section just redirects to another config
+	var ci ConfigIncluder
+	err = json.Unmarshal(sectionBytes, &ci)
+	if err == nil {
+		externalBytes := []byte{}
+		if len(ci.IncludeFromFile) > 0 {
+			externalBytes = ReadBytesFromFile(ci.IncludeFromFile)
+		} else if len(ci.IncludeFromURL) > 0 {
+			externalBytes = ReadBytesFromUrl(ci.IncludeFromURL)
+		}
+		if len(externalBytes) > 0 {
+			// redirected file contains values, merging
+			return []byte{}, MergeJsonWithDefaults(externalBytes, configStruct)
+		}
+	}
 
 	return []byte{}, MergeJsonWithDefaults(sectionBytes, configStruct)
 }
