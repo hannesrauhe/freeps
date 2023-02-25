f9aa159f8a35d6afa2b5836e5f991e1c69a31903
Hannes Rauhe <hannesrauhe@users.noreply.github.com>
2023-02-25 17:13:56 +0100

    feat: simple update script (#112)
    * simple script to update to a certain branch
    * script can be called by freeps itself
    * mostly used by myself, not meant to be a stable update path

---
+++ scripts/update-freeps.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+if [ "$(whoami)" != freeps ] ; then
+    echo "Must be run as user freeps"
+    exit 1
+fi
+
+pushd /usr/local/freeps
+
+if [ ! -d src ] ; then
+    git clone https://github.com/hannesrauhe/freeps.git src
+fi
+
+pushd src
+git clean -f
+git reset --hard HEAD
+
+BRANCH=main
+if [[ $# -ge 1 ]] ; then
+  BRANCH=$1
+elif git rev-parse --is-inside-work-tree ; then
+  BRANCH=$(git rev-parse --abbrev-ref HEAD)
+fi
+echo "Updating freeps from branch $BRANCH"
+
+git fetch --all
+git checkout $BRANCH
+git pull --ff-only
+make
+popd
+
+mkdir -p bin
+cp src/build/freepsd src/scripts/update-freeps.sh bin
+
+popd
