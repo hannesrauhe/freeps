926072e53f6c308a4bf0545320891e3bfc36041c
Hannes Rauhe <hrauhe@amazon.de>
2023-04-09 18:55:18 +0200

    chore: general cleanup of main loop

--- freepsd/freepsd.go
+++ freepsd/freepsd.go
@@ -28,6 +28,7 @@ import (
 )
 
 var verbose bool
+var configpath, fn, operator, argstring, input string
 
 type loggingConfig struct {
 	Level            logrus.Level
@@ -36,138 +37,140 @@ type loggingConfig struct {
 }
 
 func configureLogging(cr *utils.ConfigReader, logger *logrus.Logger) {
-	loggingConfig := loggingConfig{Level: logrus.InfoLevel, DisableTimestamp: false, DisableQuote: false}
+	level := logrus.InfoLevel
+	if verbose {
+		level = logrus.DebugLevel
+	}
+	loggingConfig := loggingConfig{Level: level, DisableTimestamp: false, DisableQuote: false}
 	cr.ReadSectionWithDefaults("logging", &loggingConfig)
 	logger.SetFormatter(&logrus.TextFormatter{
 		DisableTimestamp: loggingConfig.DisableTimestamp,
 		DisableQuote:     loggingConfig.DisableQuote,
 	})
-	logger.SetLevel(loggingConfig.Level)
+	if !verbose {
+		level = loggingConfig.Level
+	}
+	logger.SetLevel(level)
+}
+
+func mainLoop() bool {
+	logger := logrus.StandardLogger()
+	if verbose {
+		logger.SetLevel(logrus.DebugLevel)
+	}
+	logger.Infof("Freeps %v", utils.BuildFullVersion())
+
+	cr, err := utils.NewConfigReader(logger.WithField("component", "config"), configpath)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	configureLogging(cr, logger)
+
+	_, err = utils.GetTempDir()
+	if err != nil {
+		logger.Fatal("Temp dir creation failed: ", err.Error())
+	}
+	defer utils.DeleteTempDir()
+
+	logger.Debug("Loading graph engine")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ge := freepsgraph.NewGraphEngine(cr, cancel)
+
+	mm, err := muteme.NewMuteMe(logger, cr, ge)
+	if err != nil {
+		logger.Errorf("MuteMe not started: %v", err)
+	} else {
+		ge.AddOperator(muteme.NewMuteMeOp(mm))
+	}
+
+	//TODO(HR): load operators from config?
+	ge.AddOperator(freepsstore.NewOpStore(cr)) //needs to be first for now
+	ge.AddOperator(mqtt.NewMQTTOp(cr))
+	ge.AddOperator(telegram.NewTelegramOp(cr))
+	ge.AddOperator(freepsflux.NewFluxMod(cr))
+	ge.AddOperator(wled.NewWLEDOp(cr))
+	ge.AddOperator(ui.NewHTMLUI(cr, ge))
+	ge.AddOperator(fritz.NewOpFritz(cr))
+	freepsexec.AddExecOperators(cr, ge)
+
+	sh, err := freepsstore.NewStoreHook(cr)
+	if err != nil {
+		logger.Errorf("Store hook not available: %v", err.Error())
+	} else {
+		ge.AddHook(sh)
+	}
+
+	if err := ge.LoadEmbeddedGraphs(); err != nil {
+		logger.Fatal(err)
+	}
+
+	if operator != "" {
+		args, _ := url.ParseQuery(argstring)
+		oio := freepsgraph.MakeEmptyOutput()
+
+		if input == "-" {
+			scanner := bufio.NewScanner(os.Stdin)
+			b := []byte{}
+			for scanner.Scan() {
+				b = append(b, scanner.Bytes()...)
+			}
+			oio = freepsgraph.MakeByteOutput(b)
+		} else if input != "" {
+			content, err := ioutil.ReadFile(input)
+			if err != nil {
+				log.Fatal(err)
+			}
+			oio = freepsgraph.MakeByteOutput(content)
+		}
+		output := ge.ExecuteOperatorByName(base.NewContext(logger), operator, fn, utils.URLArgsToMap(args), oio)
+		output.WriteTo(os.Stdout)
+		return false
+	}
+
+	logger.Infof("Starting Listeners")
+	http := freepslisten.NewFreepsHttp(cr, ge)
+	m := mqtt.GetInstance()
+	if err := m.Init(logger, cr, ge); err != nil {
+		logger.Errorf("MQTT not started: %v", err)
+	} else {
+		h, _ := mqtt.NewMQTTHook(cr)
+		ge.AddHook(h)
+	}
+	fbt, err := freepsbluetooth.NewBTWatcher(logger, cr, ge)
+	if err != nil {
+		logger.Errorf("FreepsBT not started: %v", err)
+	} else if fbt != nil {
+		ge.AddHook(&freepsbluetooth.HookBluetooth{})
+	}
+	telg := telegram.NewTelegramBot(cr, ge, cancel)
+	mm.StartListening()
+
+	select {
+	case <-ctx.Done():
+		// Shutdown the server when the context is canceled
+		m.Shutdown()
+		telg.Shutdown(context.TODO())
+		http.Shutdown(context.TODO())
+		mm.Shutdown()
+	}
+	running := ge.ReloadRequested()
+	ge.Shutdown(base.NewContext(logger))
+	logger.Infof("Stopping Listeners")
+	return running
 }
 
 func main() {
-	var configpath, fn, mod, argstring, input string
 	flag.StringVar(&configpath, "c", utils.GetDefaultPath("freeps"), "Specify config file to use")
-	flag.StringVar(&mod, "m", "", "Specify mod to execute directly without starting rest server")
-	flag.StringVar(&fn, "f", "", "Specify function to execute in mod")
+	flag.StringVar(&operator, "m", "", "Specify operator to execute directly without starting listeners")
+	flag.StringVar(&fn, "f", "", "Specify the operator function to call directly")
 	flag.StringVar(&argstring, "a", "", "Specify arguments to function as urlencoded string")
 	flag.BoolVar(&verbose, "v", false, "Verbose output")
 	flag.StringVar(&input, "i", "", "input file, use \"-\" to read from stdin")
 
 	flag.Parse()
 
-	logger := logrus.StandardLogger()
-	logger.Infof("Freeps %v", utils.BuildFullVersion())
-	running := true
-	for running {
-		cr, err := utils.NewConfigReader(logger.WithField("component", "config"), configpath)
-		if err != nil {
-			logger.Fatal(err)
-		}
-		configureLogging(cr, logger)
-		if verbose {
-			logger.SetLevel(logrus.DebugLevel)
-		}
-
-		if verbose {
-			logger.SetLevel(logrus.DebugLevel)
-		}
-
-		_, err = utils.GetTempDir()
-		if err != nil {
-			logger.Fatal("Temp dir creation failed: ", err.Error())
-		}
-		defer utils.DeleteTempDir()
-
-		logger.Debug("Loading graph engine")
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		ge := freepsgraph.NewGraphEngine(cr, cancel)
-
-		mm, err := muteme.NewMuteMe(logger, cr, ge)
-		if err != nil {
-			logger.Errorf("MuteMe not started: %v", err)
-		} else {
-			ge.AddOperator(muteme.NewMuteMeOp(mm))
-		}
-
-		//TODO(HR): load operators from config?
-		ge.AddOperator(freepsstore.NewOpStore(cr)) //needs to be first for now
-		ge.AddOperator(mqtt.NewMQTTOp(cr))
-		ge.AddOperator(telegram.NewTelegramOp(cr))
-		ge.AddOperator(freepsflux.NewFluxMod(cr))
-		ge.AddOperator(wled.NewWLEDOp(cr))
-		ge.AddOperator(ui.NewHTMLUI(cr, ge))
-		ge.AddOperator(fritz.NewOpFritz(cr))
-		freepsexec.AddExecOperators(cr, ge)
-
-		sh, err := freepsstore.NewStoreHook(cr)
-		if err != nil {
-			logger.Errorf("Store hook not available: %v", err.Error())
-		} else {
-			ge.AddHook(sh)
-		}
-
-		if err := ge.LoadEmbeddedGraphs(); err != nil {
-			logger.Fatal(err)
-		}
-
-		if mod != "" {
-			args, _ := url.ParseQuery(argstring)
-			oio := freepsgraph.MakeEmptyOutput()
-
-			if input == "-" {
-				scanner := bufio.NewScanner(os.Stdin)
-				b := []byte{}
-				for scanner.Scan() {
-					b = append(b, scanner.Bytes()...)
-				}
-				oio = freepsgraph.MakeByteOutput(b)
-			} else if input != "" {
-				content, err := ioutil.ReadFile(input)
-				if err != nil {
-					log.Fatal(err)
-				}
-				oio = freepsgraph.MakeByteOutput(content)
-			}
-			output := ge.ExecuteOperatorByName(base.NewContext(logger), mod, fn, utils.URLArgsToMap(args), oio)
-			output.WriteTo(os.Stdout)
-			return
-		}
-
-		logger.Printf("Starting Listeners")
-		http := freepslisten.NewFreepsHttp(cr, ge)
-		m := mqtt.GetInstance()
-		if err := m.Init(logger, cr, ge); err != nil {
-			logger.Errorf("MQTT not started: %v", err)
-		} else {
-			h, _ := mqtt.NewMQTTHook(cr)
-			ge.AddHook(h)
-		}
-
-		fbt, err := freepsbluetooth.NewBTWatcher(logger, cr, ge)
-		if err != nil {
-			logger.Errorf("FreepsBT not started: %v", err)
-		} else if fbt != nil {
-			ge.AddHook(&freepsbluetooth.HookBluetooth{})
-		}
-		telg := telegram.NewTelegramBot(cr, ge, cancel)
-		mm.StartListening()
-
-		select {
-		case <-ctx.Done():
-			// Shutdown the server when the context is canceled
-			m.Shutdown()
-			telg.Shutdown(context.TODO())
-			http.Shutdown(context.TODO())
-			mm.Shutdown()
-			if fbt != nil {
-				fbt.Shutdown()
-			}
-		}
-		running = ge.ReloadRequested()
-		ge.Shutdown(base.NewContext(logger))
-		logger.Printf("Stopping Listeners")
+	for mainLoop() {
 	}
 }
