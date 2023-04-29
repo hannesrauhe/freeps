c83284a29e5fb000e641e35516d1773cade9053f
Hannes Rauhe <mail+github@rauhe.eu>
2023-04-29 23:23:45 +0200

    rename files

---
+++ base/baseOperator.go
@@ -0,0 +1,13 @@
+package base
+
+// FreepsBaseOperator provides the interface for all operators used by the graph module
+// Operators can either implement this interface directly or use the OperatorBuilder to convert a struct into an operator
+type FreepsBaseOperator interface {
+	Execute(ctx *Context, fn string, mainArgs map[string]string, mainInput *OperatorIO) *OperatorIO
+
+	GetFunctions() []string // returns a list of functions that this operator can execute
+	GetPossibleArgs(fn string) []string
+	GetArgSuggestions(fn string, arg string, otherArgs map[string]string) map[string]string
+	GetName() string
+	Shutdown(*Context)
+}
