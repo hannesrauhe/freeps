ebcc34ca1dbcb8a59266745bf4ff342c56653e76
Hannes Rauhe <mail+github@rauhe.eu>
2023-05-11 21:28:34 +0200

    splitting large files

---
+++ base/operatorParameters.go
@@ -0,0 +1,137 @@
+package base
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"time"
+
+	"github.com/hannesrauhe/freeps/utils"
+)
+
+func isSupportedFieldType(field reflect.Type) bool {
+	kind := field.Kind()
+	return kind == reflect.Int || kind == reflect.Int64 || kind == reflect.String || kind == reflect.Float64 || kind == reflect.Bool
+}
+
+// isSupportedField returns true if the field is a primitive type or a pointer to a primitive type
+func isSupportedField(field reflect.Value, mustBePtr bool) bool {
+	if !field.CanSet() {
+		return false
+	}
+	if field.Kind() == reflect.Ptr && mustBePtr {
+		return isSupportedFieldType(field.Type().Elem())
+	}
+	if mustBePtr {
+		return false
+	}
+	return isSupportedFieldType(field.Type())
+}
+
+// setSupportedField sets the value of the field and converts from string if necessary
+func setSupportedField(field reflect.Value, value string) error {
+	if field.Type().Kind() == reflect.Ptr {
+		newField := reflect.New(field.Type().Elem())
+		field.Set(newField)
+		field = field.Elem()
+	}
+
+	// convert the value to the type of the field, return an error if the conversion fails
+	switch field.Kind() {
+	case reflect.Int:
+		v, err := utils.StringToInt(value)
+		if err != nil {
+			return fmt.Errorf("\"%v\" is not convertible to int: %v", value, err)
+		}
+		field.SetInt(int64(v))
+	case reflect.Int64: // this might actually be a time.Duration
+		v, err := utils.StringToInt(value)
+		if err == nil {
+			field.SetInt(int64(v))
+		} else {
+			vTime, err := time.ParseDuration(value)
+			if err != nil {
+				return fmt.Errorf("\"%v\" is not convertible to int and is not a time duration: %v", value, err)
+			}
+			field.SetInt(int64(vTime))
+		}
+	case reflect.String:
+		field.SetString(value)
+	case reflect.Float64:
+		v, err := utils.StringToFloat64(value)
+		if err != nil {
+			return fmt.Errorf("\"%v\" is not convertible to float64: %v", value, err)
+		}
+		field.SetFloat(v)
+	case reflect.Bool:
+		v := utils.ParseBool(value)
+		field.SetBool(v)
+	default:
+		// should never get here
+		return fmt.Errorf("Unsupported field type: %v", field.Kind())
+	}
+	return nil
+}
+
+// SetRequiredFreepsFunctionParameters sets the parameters of the FreepsFunction based on the args map
+func (o *FreepsOperatorWrapper) SetRequiredFreepsFunctionParameters(freepsFuncParams reflect.Value, args map[string]string, failOnErr bool) *OperatorIO {
+	//make sure all non-pointer fields of the FreepsFunction are set to the values of the args map
+	for i := 0; i < freepsFuncParams.Elem().NumField(); i++ {
+		field := freepsFuncParams.Elem().Field(i)
+
+		fieldNameCase := freepsFuncParams.Elem().Type().Field(i).Name
+		fieldName := utils.StringToLower(fieldNameCase)
+		if !isSupportedField(field, false) {
+			continue
+		}
+
+		//return an error if the field is not set in the args map
+		v, ok := args[fieldName]
+		if !ok {
+			if failOnErr {
+				return MakeOutputError(http.StatusBadRequest, fmt.Sprintf("required Parameter \"%v\" is missing", fieldNameCase))
+			} else {
+				continue
+			}
+		}
+
+		// set the value of the field
+		err := setSupportedField(field, v)
+		if err != nil {
+			if failOnErr {
+				return MakeOutputError(http.StatusBadRequest, fmt.Sprintf("Parameter \"%v\" is invalid: %v", fieldNameCase, err))
+			}
+			continue
+		}
+
+		delete(args, fieldName)
+	}
+	return nil
+}
+
+// SetOptionalFreepsFunctionParameters sets the parameters of the FreepsFunction based on the args map
+func (o *FreepsOperatorWrapper) SetOptionalFreepsFunctionParameters(freepsfunc reflect.Value, args map[string]string, failOnErr bool) *OperatorIO {
+	// set all pointer fields of the FreepsFunction to the values of the args map
+	for i := 0; i < freepsfunc.Elem().NumField(); i++ {
+		field := freepsfunc.Elem().Field(i)
+
+		fieldName := utils.StringToLower(freepsfunc.Elem().Type().Field(i).Name)
+		if !isSupportedField(field, true) {
+			continue
+		}
+
+		v, ok := args[fieldName]
+		if !ok {
+			continue
+		}
+		err := setSupportedField(field, v)
+		if err != nil {
+			if failOnErr {
+				return MakeOutputError(http.StatusBadRequest, fmt.Sprintf("Parameter \"%v\" is invalid: %v", fieldName, err))
+			}
+			continue
+		}
+		delete(args, fieldName)
+	}
+	return nil
+}
