2ec40cc4607759565d1379801af7bd811e24c334
Hannes Rauhe <mail+github@rauhe.eu>
2023-05-15 21:46:13 +0200

    feat(graph): graph runtime info to store

--- connectors/store/config.go
+++ connectors/store/config.go
@@ -6,8 +6,9 @@ type StoreConfig struct {
 	PostgresSchema         string // the schema to store namespace-tables in
 	ExecutionLogInPostgres bool   // store the execution log in postgres if available
 	ExecutionLogName       string // name of the namespace for the execution log
+	GraphInfoName          string // name of the namespace for the execution log
 	ErrorLogName           string // name of the namespace for the error log
 	MaxErrorLogSize        int    // maximum number of entries in the error log
 }
 
-var defaultConfig = StoreConfig{PostgresConnStr: "", PostgresSchema: "freepsstore", ExecutionLogInPostgres: true, ExecutionLogName: "execution_log", ErrorLogName: "_error_log", MaxErrorLogSize: 1000}
+var defaultConfig = StoreConfig{PostgresConnStr: "", PostgresSchema: "freepsstore", ExecutionLogInPostgres: true, ExecutionLogName: "execution_log", GraphInfoName: "_graph_info", ErrorLogName: "_error_log", MaxErrorLogSize: 1000}
