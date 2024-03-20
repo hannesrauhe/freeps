76c1ba91f5ac2c17f6c075e25e718c9ca7680bb3
Hannes Rauhe <mail+github@rauhe.eu>
2024-03-20 22:03:15 +0100

    .

--- connectors/muteme/operator.go
+++ connectors/muteme/operator.go
@@ -3,7 +3,6 @@
 package muteme
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -44,7 +43,7 @@ func (mm *MuteMe) InitCopyOfOperator(ctx *base.Context, config interface{}, name
 	}
 
 	// Open the device using the VID and PID.
-	d, err := hid.OpenFirst(mm.config.VendorID, mm.config.ProductID)
+	d, err := hid.OpenFirst(mmc.VendorID, mmc.ProductID)
 	if err != nil {
 		return nil, err
 	}
@@ -52,156 +51,6 @@ func (mm *MuteMe) InitCopyOfOperator(ctx *base.Context, config interface{}, name
 	newMM.currentColor.Store("off")
 
 	return &newMM, nil
-}
-
-func (m *MuteMe) setColor(color string) error {
-	b := make([]byte, 2)
-	b[0] = 0x0
-
-	ok := false
-	b[1], ok = colors[color]
-	if !ok {
-		b[1] = 0x07
-		color = "white"
-	}
-
-	_, err := m.dev.Write(b)
-	lColor := m.currentColor.Load().(string)
-	if err == nil && color != lColor {
-		m.currentColor.Store(color)
-	}
-	if err != nil {
-		m.logger.Errorf("Error setting color: %v", err)
-	}
-	return err
-}
-
-func (m *MuteMe) blink(blinkColor string, afterColor string) {
-	for range []int{0, 1, 2, 3} {
-		m.setColor("off")
-		time.Sleep(100 * time.Millisecond)
-		m.setColor(blinkColor)
-		time.Sleep(100 * time.Millisecond)
-	}
-	m.setColor(afterColor)
-}
-
-func (m *MuteMe) mainloop(ge *freepsgraph.GraphEngine) {
-	bin := make([]byte, 8)
-	tpress1 := time.Now()
-	tpress2 := time.Now()
-	ignoreUntil := time.Now()
-	indicatorLightActive := false
-	longTouchLightActive := false
-	lastTouchDuration := time.Microsecond
-	lastTouchCounter := 0
-	running := true
-	color := "off"
-
-	// indicate startup by blinking:
-	m.blink(m.config.SuccessColor, color)
-
-	for running {
-		// set the user-requested color unless the indicator light is active
-		if !indicatorLightActive {
-			select {
-			case str, open := <-m.cmd:
-				if !open {
-					running = false
-				} else {
-					m.setColor(str)
-				}
-				continue
-			default:
-				// nothing to do
-			}
-		}
-		_, err := m.dev.ReadWithTimeout(bin, m.config.MultiTouchDuration)
-		if time.Now().Before(ignoreUntil) {
-			if bin[3] == 4 {
-				// make sure we don't execute something twice because the user was too slow when double touching
-				ignoreUntil = time.Now().Add(time.Second)
-			}
-			continue
-		}
-		if err != nil {
-			// should be a timeout error in normal operation
-			if !errors.Is(err, hid.ErrTimeout) {
-				// it's another error, usually interrupted system call. Nothing to do but ignore
-				// logrus.Errorf("Error getting state: %v", err)
-				continue
-			}
-
-			if lastTouchDuration <= time.Microsecond {
-				// nothing happened
-				continue
-			}
-
-			// action:
-			tags := []string{m.config.Tag}
-			args := map[string]string{}
-			if tpress2.Sub(tpress1) < m.config.MultiTouchDuration {
-				tags = append(tags, m.config.MultiTouchTag)
-				args["TouchCount"] = fmt.Sprint(lastTouchCounter)
-			} else {
-				if lastTouchDuration > m.config.LongTouchDuration {
-					tags = append(tags, m.config.LongTouchTag)
-				} else {
-					tags = append(tags, m.config.TouchTag)
-				}
-				args["TouchDuration"] = lastTouchDuration.String()
-			}
-			resultIO := ge.ExecuteGraphByTags(base.NewContext(m.logger), tags, args, base.MakeEmptyOutput())
-			ignoreUntil = time.Now().Add(time.Second)
-			m.logger.Debugf("Muteme touched, result: %v", resultIO)
-			if resultIO.IsError() {
-				m.blink(m.config.ErrorColor, color)
-			} else {
-				m.blink(m.config.SuccessColor, color)
-			}
-
-			// reset state variables
-			lastTouchDuration = time.Microsecond
-			lastTouchCounter = 0
-			indicatorLightActive = false
-			longTouchLightActive = false
-		}
-		if bin[3] == 4 { // press
-			lastTouchCounter++
-			tpress1 = tpress2
-			tpress2 = time.Now()
-			if !indicatorLightActive {
-				// make sure to not change the color multiple times
-				indicatorLightActive = true
-				color = m.getColorImpl()
-				if color != m.config.ProcessColor {
-					m.setColor(m.config.ProcessColor)
-				} else {
-					m.setColor("off")
-				}
-			}
-		}
-		if bin[3] == 2 { // release
-			lastTouchDuration = time.Now().Sub(tpress2)
-		}
-
-		if bin[3] == 1 { // pressed down
-			if time.Now().Sub(tpress2) > m.config.LongTouchDuration && !longTouchLightActive {
-				longTouchLightActive = true
-				m.setColor(m.config.LongTouchColor)
-			}
-		}
-		if bin[3] == 0 {
-		} // no touch
-	}
-
-	if err := m.dev.Close(); err != nil {
-		m.logger.Error(err)
-	}
-
-	if err := hid.Exit(); err != nil {
-		m.logger.Error(err)
-	}
 }
 
 func (m *MuteMe) setColorImpl(color string) error {
@@ -279,7 +128,19 @@ func (mm *MuteMe) Shutdown(ctx *base.Context) {
 	close(mm.cmd)
 }
 
+func (mm *MuteMe) outerLoop() {
+	running := true
+	for running {
+		mm.mainloop(&running)
+		if running {
+			//there was an error, wait a second before reinit
+			time.Sleep(time.Second)
+		}
+	}
+	mm.logger.Info("MuteMe background thread stopped")
+}
+
 // StartListening starts the main loop of the muteme listener
 func (mm *MuteMe) StartListening(ctx *base.Context) {
-	go mm.mainloop(mm.GE)
+	go mm.outerLoop()
 }
