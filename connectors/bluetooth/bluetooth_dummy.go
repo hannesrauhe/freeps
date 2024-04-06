25ec0fd463632aa8055cd732a2665d0351fb7f99
Hannes Rauhe <hannesrauhe@users.noreply.github.com>
2024-04-06 09:48:57 +0200

    chore: delete boilerplate code for dummies (#211)

--- connectors/bluetooth/bluetooth_dummy.go
+++ connectors/bluetooth/bluetooth_dummy.go
@@ -2,25 +2,6 @@
 
 package freepsbluetooth
 
-import (
-	"fmt"
+import "github.com/hannesrauhe/freeps/freepsgraph"
 
-	"github.com/hannesrauhe/freeps/base"
-	"github.com/hannesrauhe/freeps/freepsgraph"
-)
-
-type Bluetooth struct {
-	GE *freepsgraph.GraphEngine
-}
-
-var _ base.FreepsOperatorWithConfig = &Bluetooth{}
-
-// GetDefaultConfig returns a copy of the default config
-func (bt *Bluetooth) GetDefaultConfig() interface{} {
-	return &BluetoothConfig{Enabled: false}
-}
-
-// InitCopyOfOperator creates a copy of the operator and initializes it with the given config
-func (bt *Bluetooth) InitCopyOfOperator(ctx *base.Context, config interface{}, name string) (base.FreepsOperatorWithConfig, error) {
-	return nil, fmt.Errorf("Bluetooth support not compiled in")
-}
+type Bluetooth freepsgraph.DummyOperator
--- connectors/freepsflux/flux_dummy.go
+++ connectors/freepsflux/flux_dummy.go
@@ -2,21 +2,6 @@
 
 package freepsflux
 
-import (
-	"github.com/hannesrauhe/freeps/base"
-)
+import "github.com/hannesrauhe/freeps/freepsgraph"
 
-type OperatorFlux struct {
-}
-
-var _ base.FreepsOperatorWithConfig = &OperatorFlux{}
-
-// GetDefaultConfig returns a copy of the default config
-func (o *OperatorFlux) GetDefaultConfig() interface{} {
-	return &FreepsFluxConfig{Enabled: false}
-}
-
-// InitCopyOfOperator creates a copy of the operator and initializes it with the given config
-func (o *OperatorFlux) InitCopyOfOperator(ctx *base.Context, config interface{}, name string) (base.FreepsOperatorWithConfig, error) {
-	return nil, nil
-}
+type OperatorFlux freepsgraph.DummyOperator
