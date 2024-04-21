22253073371d0c530601370e88f38a4e96c18be2
Hannes Rauhe <mail+github@rauhe.eu>
2024-04-21 11:03:07 +0200

    .

--- connectors/pixeldisplay/wledSegment.go
+++ connectors/pixeldisplay/wledSegment.go
@@ -3,17 +3,17 @@ package pixeldisplay
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"image"
 	"io"
 	"net/http"
 
 	"github.com/hannesrauhe/freeps/base"
+	"github.com/hannesrauhe/freeps/utils"
 )
 
 type WLEDSegmentReqeust struct {
-	ID int         `json:"id"`
-	I  [][3]uint32 `json:"i,omitempty"`
+	ID int      `json:"id"`
+	I  []string `json:"i,omitempty"`
 }
 
 type WLEDSegmentConfig struct {
@@ -34,7 +34,6 @@ type WLEDRequest struct {
 }
 
 func newWLEDSegmentHolder(conf WLEDSegmentConfig) (*WLEDSegmentHolder, error) {
-
 	return &WLEDSegmentHolder{conf: conf, actualLen: 0}, nil
 }
 
@@ -46,10 +45,12 @@ func (h *WLEDSegmentHolder) convertImageToWLEDRequest(dst image.RGBA, jsonb WLED
 			if x&1 != 0 {
 				j = h.conf.Height - y - 1
 			}
-			r, g, b, a := dst.At(x+h.conf.OffsetX, j+h.conf.OffsetY).RGBA()
+			pixelColor := dst.At(x+h.conf.OffsetX, j+h.conf.OffsetY)
+			_, _, _, a := pixelColor.RGBA()
 			if a != 0 {
-				p := [3]uint32{r >> 8, g >> 8, b >> 8}
-				jsonb.Seg.I[outputIndex] = p
+				hc := utils.GetHexColor(pixelColor)
+				// p := [3]uint32{r >> 8, g >> 8, b >> 8}
+				jsonb.Seg.I[outputIndex] = hc[1:]
 			}
 			outputIndex++
 		}
@@ -61,34 +62,36 @@ func (h *WLEDSegmentHolder) SendToWLEDSegment(address string, dst image.RGBA, ba
 	if h.actualLen > 0 && h.conf.Width*h.conf.Height > h.actualLen {
 		return base.MakeOutputError(http.StatusBadRequest, "Array of length %v for Segment %v longer than expected the %v pixels", h.conf.Width*h.conf.Height, h.conf.SegID, h.actualLen)
 	}
-
+	segmentLength := h.conf.Width * h.conf.Height
+	if segmentLength > 256 {
+		return base.MakeOutputError(http.StatusBadRequest, "Cannot set more than 256 pixels at a time")
+	}
 	c := http.Client{}
 
 	path := address + "/json"
 
 	jsonob := WLEDRequest{}
 	jsonob.Seg.ID = h.conf.SegID
-	jsonob.Seg.I = make([][3]uint32, h.conf.Width*h.conf.Height)
+	jsonob.Seg.I = make([]string, segmentLength)
 	for _, background := range backgroundLayers {
 		h.convertImageToWLEDRequest(background, jsonob)
 	}
 	h.convertImageToWLEDRequest(dst, jsonob)
 	b, err := json.Marshal(jsonob)
 	if err != nil {
-		return base.MakeOutputError(http.StatusBadRequest, err.Error())
+		return base.MakeOutputError(http.StatusInternalServerError, "Error when trying to prepare request for wled: %v", err.Error())
 	}
 	breader := bytes.NewReader(b)
 	resp, err := c.Post(path, "application/json", breader)
 
 	if err != nil {
-		return base.MakeOutputError(http.StatusInternalServerError, "%v", err.Error())
+		return base.MakeOutputError(http.StatusInternalServerError, "Error when trying to send request to wled: %v", err.Error())
 	}
 
 	defer resp.Body.Close()
 	bout, err := io.ReadAll(resp.Body)
 	if err != nil {
-		// TODO(HR): error handling
-		fmt.Printf("\n%v\n", err)
+		return base.MakeOutputError(http.StatusInternalServerError, "Error when trying to read response from wled: %v", err)
 	}
 	return &base.OperatorIO{HTTPCode: resp.StatusCode, Output: bout, OutputType: base.Byte, ContentType: resp.Header.Get("Content-Type")}
 }
