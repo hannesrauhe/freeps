73bcc96ba9f8058216d776f5352ded9c85f66e83
Hannes Rauhe <hannes.rauhe@pik-potsdam.de>
2024-12-26 17:22:09 +0100

    chore(flowbuilder): mostly untested functionality to edit flows

---
+++ connectors/flowbuilder/editFlows.go
@@ -0,0 +1,76 @@
+package flowbuilder
+
+import (
+	"github.com/hannesrauhe/freeps/base"
+	freepsstore "github.com/hannesrauhe/freeps/connectors/store"
+	"github.com/hannesrauhe/freeps/freepsflow"
+)
+
+// FlowFromStoreArgs are the arguments for the FlowBuilder function
+type FlowFromStoreArgs struct {
+	FlowName        string
+	CreateIfMissing *bool
+}
+
+// FlowNameSuggestions returns suggestions for flow names
+func (arg *FlowFromStoreArgs) FlowNameSuggestions(m *OpFlowBuilder) []string {
+	flowNames := []string{}
+	res := freepsstore.GetFlowStore().GetAllValues(30)
+	for name := range res {
+		flowNames = append(flowNames, name)
+	}
+	return flowNames
+}
+
+func (m *OpFlowBuilder) buildDefaultOperation() freepsflow.FlowOperationDesc {
+	return freepsflow.FlowOperationDesc{
+		Operator:  "system",
+		Function:  "noop",
+		Arguments: map[string]string{},
+	}
+}
+
+// RestoreDeletedFlowFromStore restores a flow from the backup in store
+func (m *OpFlowBuilder) RestoreDeletedFlowFromStore(ctx *base.Context, input *base.OperatorIO, args FlowFromStoreArgs) *base.OperatorIO {
+	gd, err := freepsstore.GetFlow("deleted_" + args.FlowName)
+	if err != nil {
+		return base.MakeOutputError(400, "Could not restore flow: %v", err)
+	}
+	err = m.GE.AddFlow(ctx, args.FlowName, gd, false)
+	if err != nil {
+		return base.MakeOutputError(400, "Could not restore flow: %v", err)
+	}
+	return base.MakeEmptyOutput()
+}
+
+// ExecuteFlowFromStore executes a flow after loading it from the store
+func (m *OpFlowBuilder) ExecuteFlowFromStore(ctx *base.Context, input *base.OperatorIO, args FlowFromStoreArgs) *base.OperatorIO {
+	gd, err := freepsstore.GetFlow(args.FlowName)
+	if err != nil {
+		return base.MakeOutputError(404, "Flow not found in store: %v", err)
+	}
+	return m.GE.ExecuteAdHocFlow(ctx, "ExecuteFromStore/"+args.FlowName, gd, base.MakeEmptyFunctionArguments(), input)
+}
+
+// GetFlowFromStore returns a flow from the store
+func (m *OpFlowBuilder) GetFlowFromStore(ctx *base.Context, input *base.OperatorIO, args FlowFromStoreArgs) *base.OperatorIO {
+	gd, err := freepsstore.GetFlow(args.FlowName)
+	if err != nil {
+		gd = freepsflow.FlowDesc{}
+		if args.CreateIfMissing == nil || !*args.CreateIfMissing {
+			return base.MakeOutputError(404, "Flow not found in store: %v", err)
+		}
+		return freepsstore.StoreFlow(args.FlowName, gd, ctx)
+	}
+	return base.MakeObjectOutput(gd)
+}
+
+// CreateFlowInStore creates a new flow in the store
+func (m *OpFlowBuilder) CreateFlowInStore(ctx *base.Context, input *base.OperatorIO, args FlowFromStoreArgs) *base.OperatorIO {
+	gd, err := freepsstore.GetFlow(args.FlowName)
+	if err == nil {
+		return base.MakeOutputError(400, "Flow already exists in store: %v", args.FlowName)
+	}
+	gd = freepsflow.FlowDesc{}
+	return freepsstore.StoreFlow(args.FlowName, gd, ctx)
+}
---
+++ connectors/flowbuilder/operations.go
@@ -0,0 +1,123 @@
+package flowbuilder
+
+import (
+	"github.com/hannesrauhe/freeps/base"
+	freepsstore "github.com/hannesrauhe/freeps/connectors/store"
+	"github.com/hannesrauhe/freeps/freepsflow"
+)
+
+// AddOperationArgs are the arguments for the AddOperation function, number and all OperationDesc fields are optional
+type AddOperation struct {
+	FlowName        string
+	OperationNumber *int
+	OperationName   *string
+	Operator        *string
+	Function        *string
+	InputFrom       *string
+	ExecuteOnFailOf *string
+	ArgumentsFrom   *string
+	UseMainArgs     *bool
+}
+
+// AddOperation adds an operation to a flow in the store
+func (m *OpFlowBuilder) AddOperation(ctx *base.Context, input *base.OperatorIO, args AddOperation) *base.OperatorIO {
+	gd, err := freepsstore.GetFlow(args.FlowName)
+	if err != nil {
+		return base.MakeOutputError(404, "Flow not found in store: %v", err)
+	}
+	operationNumber := len(gd.Operations)
+	if args.OperationNumber != nil {
+		operationNumber = *args.OperationNumber
+	}
+
+	operationDesc := m.buildDefaultOperation()
+	if args.OperationName != nil {
+		operationDesc.Name = *args.OperationName
+	}
+	if args.Operator != nil {
+		operationDesc.Operator = *args.Operator
+	}
+	if args.Function != nil {
+		operationDesc.Function = *args.Function
+	}
+	if args.InputFrom != nil {
+		operationDesc.InputFrom = *args.InputFrom
+	}
+	if args.ExecuteOnFailOf != nil {
+		operationDesc.ExecuteOnFailOf = *args.ExecuteOnFailOf
+	}
+	if args.ArgumentsFrom != nil {
+		operationDesc.ArgumentsFrom = *args.ArgumentsFrom
+	}
+	if args.UseMainArgs != nil {
+		operationDesc.UseMainArgs = *args.UseMainArgs
+	}
+	if operationNumber < 0 || operationNumber > len(gd.Operations) {
+		gd.Operations = append(gd.Operations, operationDesc)
+	} else {
+		gd.Operations = append(gd.Operations[:operationNumber], append([]freepsflow.FlowOperationDesc{operationDesc}, gd.Operations[operationNumber:]...)...)
+	}
+	return freepsstore.StoreFlow(args.FlowName, gd, ctx)
+}
+
+// SetOperationArgs sets the fields of an operation given by the number in a flow in the store
+type SetOperationArgs struct {
+	FlowName        string
+	OperationNumber int
+	Operator        *string
+	Function        *string
+	ArgumentName    *string
+	ArgumentValue   *string
+}
+
+// SetOperation sets the fields of an operation given by the number in a flow in the store
+func (m *OpFlowBuilder) SetOperation(ctx *base.Context, input *base.OperatorIO, args SetOperationArgs) *base.OperatorIO {
+	gd, err := freepsstore.GetFlow(args.FlowName)
+	if err != nil {
+		return base.MakeOutputError(404, "Flow not found in store: %v", err)
+	}
+	if args.OperationNumber < 0 || args.OperationNumber > len(gd.Operations) {
+		return base.MakeOutputError(400, "Invalid operation number")
+	}
+	if args.OperationNumber == len(gd.Operations) {
+		gd.Operations = append(gd.Operations, m.buildDefaultOperation())
+	}
+
+	if args.Operator != nil {
+		gd.Operations[args.OperationNumber].Operator = *args.Operator
+	}
+	if args.Function != nil {
+		gd.Operations[args.OperationNumber].Function = *args.Function
+	}
+	if args.ArgumentName != nil {
+		if args.ArgumentValue == nil {
+			return base.MakeOutputError(400, "Argument value is missing")
+		}
+		gd.Operations[args.OperationNumber].Arguments[*args.ArgumentName] = *args.ArgumentValue
+	}
+	return freepsstore.StoreFlow(args.FlowName, gd, ctx)
+}
+
+// RemoveOperationArgs are the arguments for the RemoveOperation function
+type RemoveOperationArgs struct {
+	FlowName        string
+	OperationNumber int
+}
+
+// RemoveOperation removes an operation from a flow in the store
+func (m *OpFlowBuilder) RemoveOperation(ctx *base.Context, input *base.OperatorIO, args RemoveOperationArgs) *base.OperatorIO {
+	gd, err := freepsstore.GetFlow(args.FlowName)
+	if err != nil {
+		return base.MakeOutputError(404, "Flow not found in store: %v", err)
+	}
+	if args.OperationNumber == len(gd.Operations)-1 {
+		gd.Operations = gd.Operations[:args.OperationNumber]
+	} else if args.OperationNumber == 0 {
+		gd.Operations = gd.Operations[1:]
+	} else if args.OperationNumber < len(gd.Operations)-1 {
+		gd.Operations = append(gd.Operations[:args.OperationNumber], gd.Operations[args.OperationNumber+1:]...)
+	} else {
+		return base.MakeOutputError(400, "Invalid operation number")
+	}
+	return freepsstore.StoreFlow(args.FlowName, gd, ctx)
+}
