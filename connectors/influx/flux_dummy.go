51db23dbdb78fc1580a3fe98e039ce42c80eebb0
Hannes Rauhe <mail+github@rauhe.eu>
2025-04-06 22:22:03 +0200

    .

--- connectors/influx/flux_dummy.go
+++ connectors/influx/flux_dummy.go
@@ -2,8 +2,15 @@
 
 package influx
 
-import "github.com/hannesrauhe/freeps/freepsflow"
+import (
+	"github.com/hannesrauhe/freeps/base"
+	"github.com/hannesrauhe/freeps/freepsflow"
+)
 
 type OperatorInflux freepsflow.DummyOperator
 
 var GlobalOperatorInflux *OperatorInflux = nil
+
+func (o *OperatorInflux) PushFieldsInternal(measurement string, tags map[string]string, fields map[string]interface{}, ctx *base.Context) *base.OperatorIO {
+	panic("InfluxDB db support not available, this should not be called")
+}
