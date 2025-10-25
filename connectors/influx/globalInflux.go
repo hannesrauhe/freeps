d4d3a3df24068f8f2f2a882cfcede59e281a1dff
Hannes Rauhe <mail+github@rauhe.eu>
2025-10-25 13:39:17 +0200

    comment fix

--- connectors/influx/globalInflux.go
+++ connectors/influx/globalInflux.go
@@ -2,7 +2,7 @@ package influx
 
 var globalInflux map[string]*OperatorInflux
 
-// GetGlobalInfluxInstance returns the global sensor instance, that can be used by other operators to manage their sensors
+// GetGlobalInfluxInstance returns the global Influx instance with the given name, enabling other operators to use influx directly
 func GetGlobalInfluxInstance(instanceName string) *OperatorInflux {
 	if globalInflux == nil {
 		return nil
